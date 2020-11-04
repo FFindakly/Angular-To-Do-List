10427a3730a74e68dc872824e18f11f73bd8eb18
fadi Findakly <fadi.findakly01@stclairconnect.ca>
2020-11-03 22:06:52 -0500

    Implemented todosLimit

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+// Properties
+todsUrl = "https://jsonplaceholder.typicode.com/todos";
+todosLimit = "?_limit=10";
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo 1',
-        completed: false
-      },
-            {
-        id: 2,
-        title: 'Todo 2',
-        completed: true
-      },
-            {
-        id: 3,
-        title: 'Todo 3',
-        completed: false
-      },
-    ];
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todsUrl}${this.todosLimit}`);
+
+    }
   }
-}
