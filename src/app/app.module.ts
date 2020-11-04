ba3f89dfd065dab002d94314d0660c0685d8ee8b
fadi Findakly <fadi.findakly01@stclairconnect.ca>
2020-11-03 20:28:33 -0500

    Implemenetd the input decorator to apply *ngFor on the todo item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
 import { TodosComponent } from './components/todos/todos.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
