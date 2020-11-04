4618df57a33d81e41659e2b529d007eeddc69235
fadi Findakly <fadi.findakly01@stclairconnect.ca>
2020-11-03 19:09:52 -0500

    Cleaned the default html file for app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo-list';
+  name: string = 'Fadi';
+
+  constructor() {
+    this.changeName("Emma");
+  }
+
+  changeName(newName: string): void {
+    this.name = newName; 
+  }
 }
